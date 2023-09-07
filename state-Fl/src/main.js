7eae17bbb58bb35895d1c99fca7a563c0d2e626b
Dealer03 <crazydealer2003@protonmail.com>
2023-09-07 00:07:53 -0400

    Creates and formats the sidebar

--- state-Fl/src/main.js
+++ state-Fl/src/main.js
@@ -1,2 +1,4 @@
 import '../styles.css/normalize-modern.css'
-import '../styles.css/style.css';
+import '../styles.css/style.css'
+import '../styles.css/components/sidebar.css'
+import '../styles.css/utils.css'
