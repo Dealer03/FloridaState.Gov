a478568cde8fa6c4edf7547befcf8aa8b76d9c43
Dealer03 <crazydealer2003@protonmail.com>
2023-09-09 13:21:24 -0400

    Fixes bug in the header layout

--- state-Fl/src/main.js
+++ state-Fl/src/main.js
@@ -1,4 +1,9 @@
 import '../styles.css/style.css'
 import '../styles.css/modernize-normalize.css'
 import '../styles.css/components/header.css'
+import '../styles.css/components/mobile-nav.css'
 import '../styles.css/utils.css'
+
+import mobileNav from './utils/mobile-nav'
+
+mobileNav()
