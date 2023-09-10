cee85b26d2883ce39e20d082a86fc06152114fe3
Dealer03 <crazydealer2003@protonmail.com>
2023-09-10 11:08:05 -0400

    Adds a menu to the sidebar

--- state-Fl/src/main.js
+++ state-Fl/src/main.js
@@ -3,6 +3,7 @@ import '../styles.css/modernize-normalize.css'
 import '../styles.css/components/header.css'
 import '../styles.css/components/mobile-nav.css'
 import '../styles.css/components/home.css'
+import '../styles.css/components/sidebar.css'
 import '../styles.css/utils.css'
 
 import mobileNav from './utils/mobile-nav'
