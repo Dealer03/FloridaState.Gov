e1ead7ba7688cbd90804a016046a7f8986133d8a
Dealer03 <crazydealer2003@protonmail.com>
2023-09-10 10:24:00 -0400

    Creates and configures the home section

--- state-Fl/src/main.js
+++ state-Fl/src/main.js
@@ -2,6 +2,7 @@ import '../styles.css/style.css'
 import '../styles.css/modernize-normalize.css'
 import '../styles.css/components/header.css'
 import '../styles.css/components/mobile-nav.css'
+import '../styles.css/components/home.css'
 import '../styles.css/utils.css'
 
 import mobileNav from './utils/mobile-nav'
